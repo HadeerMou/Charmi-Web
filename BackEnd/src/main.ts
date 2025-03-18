5ee4dda1fef554c2c9a7c66411d773d9ef87a6c4
hadeer <109919062+HadeerMou@users.noreply.github.com>
2025-03-18 03:03:54 +0200

    upload

--- BackEnd/src/main.ts
+++ BackEnd/src/main.ts
@@ -12,25 +12,35 @@ async function bootstrap() {
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
 
+  // Configure Swagger
   const config = new DocumentBuilder()
     .setTitle('Charmi API')
     .setDescription('The Charmi API description')
     .addServer(`http://localhost:${port}`)
-    .addServer('https://api.charmi-eg.com')
-    .addServer('https://charmi.awsapprunner.com')
+    .addServer('https://charmi-web-production.up.railway.app')
+    .addServer('https://api.charmi-eg.com') // Modify as needed
     .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('swagger', app, document);
 
-  app.enableCors();
+  // Configure CORS properly
+  app.enableCors({
+    origin: [
+      'http://localhost:3001',
+      'https://charmi-web-production.up.railway.app',
+    ],
+    methods: 'GET,POST,PUT,DELETE,PATCH,OPTIONS',
+    allowedHeaders: 'Content-Type,Authorization',
+    credentials: true,
+  });
 
   app.enableShutdownHooks();
 
   await app.listen(port);
+  console.log(`Application is running on: http://localhost:${port}`);
 }
-bootstrap()
-  .then(() =>
-    console.log(`Application is running on: http://localhost:${3000}`),
-  )
-  .catch((error) => console.error('Error during application startup:', error));
+
+bootstrap().catch((error) =>
+  console.error('Error during application startup:', error),
+);
